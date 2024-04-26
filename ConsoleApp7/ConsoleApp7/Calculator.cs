198dbc5cee84a076073870acef08f0cdaecc08dd
vova836 <123488703+vova836@users.noreply.github.com>
2024-04-26 13:49:49 +0300

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    /// <summary>
+    /// Создаем обобщенный класс Калькулятор
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Calculator<T>
+    {
+        /// <summary>
+        /// Создаем конструктор который принимает два параметра любого типа T и преобразует в динамический тип, а после прибавляет числа
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public T Add(T a, T b)
+        {
+            dynamic value1 = a;
+            dynamic value2 = b;
+            return value1 + value2;
+        }
+        /// <summary>
+        /// Создаем конструктор который принимает два параметра любого типа T и преобразует в динамический тип, а после отнимаем числа
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public T Subtract(T a, T b)
+        {
+            dynamic value1 = a;
+            dynamic value2 = b;
+            return value1 - value2;
+        }
+        /// <summary>
+        /// Создаем конструктор который принимает два параметра любого типа T и преобразует в динамический тип, а после умножаем числа
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public T Multiply(T a, T b)
+        {
+            dynamic value1 = a;
+            dynamic value2 = b;
+            return value1 * value2;
+        }
+        /// <summary>
+        /// Создаем конструктор который принимает два параметра любого типа T и преобразует в динамический тип, а после разделяем числа
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public T Divide(T a, T b)
+        {
+            dynamic value1 = a;
+            dynamic value2 = b;
+            return value1 / value2;
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача2.Необходимио создать обобщенный класс калькулятора с методом с одним параметрам, нам нужно в классе програм создать первый обьект у которого один параметр целым и второй целый, 
+            // следующий объекте у первого целый второй вещественный
+            // и третий обьект перый целый второй строковый.
+            // Создаем первый объект с параметрами целыми числами
+            Calculator<int> intCalculator = new Calculator<int>();
+            Console.WriteLine("Целочисленные значения:");
+            Console.WriteLine("Сложение: " + intCalculator.Add(5, 3));
+            Console.WriteLine("Вычитание: " + intCalculator.Subtract(5, 4));
+            Console.WriteLine("Умножение: " + intCalculator.Multiply(6, 3));
+            Console.WriteLine("Деление: " + intCalculator.Divide(4, 2));
+
+            // Создаем второй объект с первым параметром целым и вторым вещественным числом
+            Calculator<double> doubleCalculator = new Calculator<double>();
+            Console.WriteLine("\nЦелое и вещественное значения:");
+            Console.WriteLine("Сложение: " + doubleCalculator.Add(5, 3.5));
+            Console.WriteLine("Вычитание: " + doubleCalculator.Subtract(5, 2.8));
+            Console.WriteLine("Умножение: " + doubleCalculator.Multiply(3, 3.7));
+            Console.WriteLine("Деление: " + doubleCalculator.Divide(2, 6.4));
+
+            // Создаем третий объект с первым параметром целым и вторым строкой
+            Calculator<string> stringCalculator = new Calculator<string>();
+            Console.WriteLine("\nЦелое и строковое значения:");
+            Console.WriteLine("Сложение: " + stringCalculator.Add("5", "3"));
+            // Поддерживаем только сложение для строк
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
